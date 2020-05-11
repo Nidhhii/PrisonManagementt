f46e2fb89cb0d962b0f1449df694d52d1bed2d91
Nidhhii <47696016+Nidhhii@users.noreply.github.com>
2020-05-11 17:32:53 +0530

    skm

--- cpanel/cpanel/settings.py
+++ cpanel/cpanel/settings.py
@@ -118,3 +118,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'mahimap7@gmail.com'
+EMAIL_HOST_PASSWORD = '****'
--- cpanel/cpanel/views.py
+++ cpanel/cpanel/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.contrib import auth
+from django.http import HttpResponse
+from django.core.mail import BadHeaderError, send_mail
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.mail import EmailMessage
+from django.conf import settings
+from django.core.mail import BadHeaderError, send_mail
+from django.http import HttpResponse, HttpResponseRedirect
 import pyrebase
 config={
     
@@ -259,20 +266,17 @@ def postaddvisitor(request):
     name=request.POST.get('vname')
     photo=request.POST.get('img4')
     gender=request.POST.get('gender')
+    email=request.POST.get('email')
     prisonerID=request.POST.get('id')
     address=request.POST.get('address')
     state=request.POST.get('state')
     pincode=request.POST.get('pincode')
     url4=request.POST.get('url4')
-    idtoken=request.session['uid']
-    a=authe.get_account_info(idtoken)
-    a=a['users']
-    a=a[0]
-    a=a['localId']
     data = {
         "Name": name,
         "photo": photo,
         "gender": gender,
+        "email": email,
         "prisonerID": prisonerID,
         "address": address,
         "state": state,
@@ -280,7 +284,7 @@ def postaddvisitor(request):
         "photo": url4
 
     }
-    database.child('users').child(a).child('info').child('visitors').child(millis).set(data)
+    database.child('users').child('visitors').child(millis).set(data)
 
     return render(request,"signIn.html", )
 
@@ -292,7 +296,7 @@ def viewVisitors(request):
     a = a['users']
     a = a[0]
     a = a['localId']
-    timestamps=database.child("users").child(a).child('info').child('visitors').shallow().get().val()
+    timestamps=database.child("users").child('visitors').shallow().get().val()
 
     lis_time=[]
     for i in timestamps:
@@ -301,8 +305,7 @@ def viewVisitors(request):
     name=[]
 
     for i in lis_time:
-        nam=database.child('users').child(a).child('info').child('visitors').child(i).child('Name').get().val()
-        print(nam)
+        nam=database.child('users').child('visitors').child(i).child('Name').get().val()
         name.append(nam)
     date=[]
     for i in lis_time:
@@ -323,13 +326,127 @@ def post_check3(request):
     a = a['users']
     a = a[0]
     a = a['localId']
-    name = database.child('users').child(a).child('info').child('visitors').child(time).child('Name').get().val()
-    print(name)
-    photo = database.child('users').child(a).child('info').child('visitors').child(time).child('photo').get().val()
-    id = database.child('users').child(a).child('info').child('visitors').child(time).child('prisonerID').get().val()
-    gender = database.child('users').child(a).child('info').child('visitors').child(time).child('gender').get().val()
-    address = database.child('users').child(a).child('info').child('visitors').child(time).child('address').get().val()
-    state = database.child('users').child(a).child('info').child('visitors').child(time).child('state').get().val()
-    pincode = database.child('users').child(a).child('info').child('visitors').child(time).child('pincode').get().val()
+    name = database.child('users').child('visitors').child(time).child('Name').get().val()
+    photo = database.child('users').child('visitors').child(time).child('photo').get().val()
+    id = database.child('users').child('visitors').child(time).child('prisonerID').get().val()
+    gender = database.child('users').child('visitors').child(time).child('gender').get().val()
+    email = database.child('users').child('visitors').child(time).child('email').get().val()
+    print(email)
+    address = database.child('users').child('visitors').child(time).child('address').get().val()
+    state = database.child('users').child('visitors').child(time).child('state').get().val()
+    pincode = database.child('users').child('visitors').child(time).child('pincode').get().val()
 
-    return render(request,'post_check3.html',{'name':name,'photo':photo,'id':id,'gender':gender,'address':address,'state':state,'pincode':pincode})
+    return render(request,'post_check3.html',{'name':name,'photo':photo,'id':id,'gender':gender,'email':email,'address':address,'state':state,'pincode':pincode})
+
+def post_accept(request):
+    import datetime
+    time=request.GET.get('z')
+    idtoken = request.session['uid']
+    a = authe.get_account_info(idtoken)
+    a = a['users']
+    a = a[0]
+    a = a['localId']
+    email = database.child('users').child('visitors').child(time).child('email').get().val()
+    print(email)
+    database.child('users').child('visitors').child(time).remove()
+    send_mail("Your visit has been accepted.", "Your visit has been accepted. Your visit has been scheduled on 05.06.2020.\nVisiting timings is from 10 AM to 2 PM. \nFollow all rules of protocol",'mahimap7@gmail.com', [email])
+    return render(request,"welcome.html")
+
+def post_reject(request):
+    import datetime
+    time=request.GET.get('z')
+    idtoken = request.session['uid']
+    a = authe.get_account_info(idtoken)
+    a = a['users']
+    a = a[0]
+    a = a['localId']
+    print("hiii")
+    email = database.child('users').child('visitors').child(time).child('email').get().val()
+    print(email)
+    database.child('users').child('visitors').child(time).remove()
+    send_mail("Your visit has been rejected.", "Your visit has been rejected. Please try again later",'mahimap7@gmail.com', [email])
+    return render(request,"welcome.html")
+def index(request):
+    return render(request,"index.html")
+
+def about(request):
+    return render(request,"about.html")
+
+def blog(request):
+    return render(request,"blog.html")
+
+def blog_single(request):
+    return render(request,"blog-single.html")
+
+def contact(request):
+    return render(request,"contact.html")
+
+def postcontact(request):
+    name = request.POST.get('name')
+    email = request.POST.get('email')
+    subject = request.POST.get('subject')
+    message = request.POST.get('message')
+    if(name and email and subject and message):
+        send_mail(subject, ("Name: " +name+  "\n") + message, 'mahimap7@gmail.com', [email])
+        mess=('Message sent.')
+        return render(request,"contact.html",{"mess":mess})
+    else:
+        message=("Please fill in all the fields.")
+        return render(request,"contact.html",{"message":message})
+    
+
+def portfolio(request):
+    return render(request,"portfolio.html")
+
+def services(request):
+    return render(request,"services.html")
+
+def vendor_partner(request):
+    return render(request,"vendor_partner.html")
+
+def postvendor(request):
+    company_name = request.POST.get('company_name')
+    name = request.POST.get('name')
+    email = request.POST.get('email')
+    phone = request.POST.get('phone')
+    company_address = request.POST.get('company_address')
+    services_offered = request.POST.get('services_offered')
+    notes = request.POST.get('notes')
+    if(company_name and name and email and phone and company_address ):
+        mail = EmailMessage(company_name, services_offered, settings.EMAIL_HOST_USER , [email])
+        mail.content_subtype = "html"
+        catalog = request.FILES['catalog']
+        mail.attach(catalog.name, catalog.read(), catalog.content_type)
+        mail.send()
+        mess=('Message sent.')
+        return render(request,"vendor_partner.html",{"mess":mess})
+    else:
+        message=("Please fill in all the fields.")
+        return render(request,"vendor_partner.html",{"message":message})
+
+def postdesign(request):
+    name = request.POST.get('name')
+    email = request.POST.get('email')
+    phone = request.POST.get('phone')
+    present_address = request.POST.get('present_address')
+    permanent_address = request.POST.get('permanent_address')
+    resume = request.FILES['resume']
+    portfolio = request.FILES['portfolio']
+    notes = request.POST.get('notes')
+    if(name and email and phone and present_address and resume and portfolio):
+        mail = EmailMessage(name, phone, settings.EMAIL_HOST_USER , [email])
+        mail.content_subtype = "html"
+        mail.attach(resume.name, resume.read(), resume.content_type)
+        mail.attach(portfolio.name, portfolio.read(), portfolio.content_type)
+        mail.send()
+        mess=('Message sent.')
+        return render(request,"design_partner.html",{"mess":mess})
+    else:
+        message=("Please fill in all the fields.")
+        return render(request,"design_partner.html",{"message":message})
+
+def design_partner(request):
+    return render(request,"design_partner.html")
+
+def team(request):
+    return render(request,"team.html")
